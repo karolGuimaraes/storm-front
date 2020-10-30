5a516fcca1804dace2351f54996ccf3743a67359
Karolinny Guimaraes <karolinny.silva@gmail.com>
2020-10-30 08:14:27 -0300

    Conversion of Class Component to a Function Component

--- src/components/TableUser/index.js
+++ src/components/TableUser/index.js
@@ -13,111 +13,103 @@ import {
   TableRow,
 } from '@material-ui/core';
 
+export default function TableUser () {
+  const [selected, setSelected] = useState([]);
 
-export default class TableUser extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { selected : [] }
+  const isSelected = (username) => {
+    return selected.indexOf(username) !== -1
   }
 
-  isSelected(username) { 
-    return this.state.selected.indexOf(username) !== -1
-  }
-
-  handleClick (event, username) {
-    const selectedIndex = this.state.selected.indexOf(username);
+  const onClickHandler = (username) => {
+    const selectedIndex = selected.indexOf(username);
     if (selectedIndex === -1) {
-      this.setState({selected: username});
-    } else if (selectedIndex === 0) {
-      this.setState({selected: []});
-    } else if (selectedIndex === this.state.selected.length - 1) {
-      this.setState({selected: []});
+      setSelected(username);
+    } else  {
+      setSelected([]);
     }
   }
   
-  render() {
-    return (
-      <>
-        <div className={styles.containerTable}>
-          <Table className={styles.contentTable}>
-            <TableHead>
-              <TableRow>
-                <TableCell align="center"></TableCell>
-                <TableCell align="center"></TableCell>
-                <TableCell align="left">USUÁRIO</TableCell>
-                <TableCell align="left">EMAIL</TableCell>
-                <TableCell align="center">DATA DE INCLUSÃO</TableCell>
-                <TableCell align="center">DATA DE ALTERAÇÃO</TableCell>
-                <TableCell align="center">REGRAS</TableCell>
-                <TableCell align="center">STATUS</TableCell>
-                <TableCell align="center"></TableCell>
-                <TableCell align="center">AÇÕES</TableCell>
-              </TableRow>
-            </TableHead>
+  return (
+    <>
+      <div className={styles.containerTable}>
+        <Table className={styles.contentTable}>
+          <TableHead>
+            <TableRow>
+              <TableCell align="center"></TableCell>
+              <TableCell align="center"></TableCell>
+              <TableCell align="left">USUÁRIO</TableCell>
+              <TableCell align="left">EMAIL</TableCell>
+              <TableCell align="center">DATA DE INCLUSÃO</TableCell>
+              <TableCell align="center">DATA DE ALTERAÇÃO</TableCell>
+              <TableCell align="center">REGRAS</TableCell>
+              <TableCell align="center">STATUS</TableCell>
+              <TableCell align="center"></TableCell>
+              <TableCell align="center">AÇÕES</TableCell>
+            </TableRow>
+          </TableHead>
 
-            <TableBody>
-              {users.map((user, index) => { return(
-                <TableRow
-                  hover
-                  onClick={(event) => this.handleClick(event, user.username)}
-                  role="checkbox"
-                  aria-checked={this.isSelected(user.username)}
-                  tabIndex={-1}
-                  key={user.username}
-                  selected={this.isSelected(user.username)}
-                  style={{
-                    backgroundColor: index % 2 === 0 ? '#F5F5F5' : '#E9E9E9',
-                  }}
-                  className={this.isSelected(user.username) ? styles.selected : styles.row }
-                >
-                <div className="overlay"></div>
-                <TableCell>
-                  <Checkbox checked={this.isSelected(user.username)} />
+          <TableBody>
+            {users.map((user, index) => { return(
+              <TableRow
+                hover
+                onClick={(event) => onClickHandler(user.username)}
+                role="checkbox"
+                aria-checked={isSelected(user.username)}
+                tabIndex={-1}
+                key={user.username}
+                selected={isSelected(user.username)}
+                style={{
+                  backgroundColor: index % 2 === 0 ? '#F5F5F5' : '#E9E9E9',
+                }}
+                className={isSelected(user.username) ? styles.selected : styles.row }
+              >
+              <div className="overlay"></div>
+              <TableCell>
+                <Checkbox checked={isSelected(user.username)} />
+              </TableCell>
+              <TableCell align="left">{user.username}</TableCell>
+              <TableCell align="left">{user.email}</TableCell>
+              <TableCell align="center">{user.inclusion}</TableCell>
+              <TableCell align="center">{user.change}</TableCell>
+              <TableCell align="center">{user.rules}</TableCell>
+              { user.status ?
+                <TableCell align="center" style={{ color: 'green' }}>
+                  ATIVO
                 </TableCell>
-                <TableCell align="left">{user.username}</TableCell>
-                <TableCell align="left">{user.email}</TableCell>
-                <TableCell align="center">{user.inclusion}</TableCell>
-                <TableCell align="center">{user.change}</TableCell>
-                <TableCell align="center">{user.rules}</TableCell>
-                { user.status ?
-                  <TableCell align="center" style={{ color: 'green' }}>
-                    ATIVO
-                  </TableCell>
-                :
-                  <TableCell align="center" style={{ color: 'red' }}>
-                    INATIVO
-                  </TableCell>
-                }
+              :
+                <TableCell align="center" style={{ color: 'red' }}>
+                  INATIVO
+                </TableCell>
+              }
 
-                <TableCell align="center" className={this.isSelected(user.username) ? styles.utilsSelected : styles.utils }>
-                  <button>
-                    <DeleteIcon />
-                  </button>
-                  <button>
-                    <EditIcon />
-                  </button>
-                </TableCell>
+              <TableCell align="center" className={isSelected(user.username) ? styles.utilsSelected : styles.utils }>
+                <button>
+                  <DeleteIcon />
+                </button>
+                <button>
+                  <EditIcon />
+                </button>
+              </TableCell>
 
-                <TableCell align="center">
-                  <button style={{border:'none',cursor: 'pointer'}}>
-                    <MoreHorizIcon />
-                  </button>
-                </TableCell>
-              </TableRow>
-              )})}
-            </TableBody>
-          </Table>
-        </div>
-        <div className={styles.containerPagination}>
-          <button className={styles.btnPage}>Primeiro</button>
-          <button className={styles.btnPage}>Anterior</button>
-          <button variant="contained" color="secondary" className={styles.current}>
-            1
-          </button>
-          <button className={styles.btnPage}>Próximo</button>
-          <button className={styles.btnPage}>Último</button>
-        </div>
-      </>
-      )
-    }
-  }
+              <TableCell align="center">
+                <button style={{border:'none',cursor: 'pointer'}}>
+                  <MoreHorizIcon />
+                </button>
+              </TableCell>
+            </TableRow>
+            )})}
+          </TableBody>
+        </Table>
+      </div>
+      <div className={styles.containerPagination}>
+        <button className={styles.btnPage}>Primeiro</button>
+        <button className={styles.btnPage}>Anterior</button>
+        <button variant="contained" color="secondary" className={styles.current}>
+          1
+        </button>
+        <button className={styles.btnPage}>Próximo</button>
+        <button className={styles.btnPage}>Último</button>
+      </div>
+    </>
+  )  
+}
